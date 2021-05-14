af4d9236cfc3ed9d1b10eccca6502283a6c24e35
王郁 <wangyu@wycode.cn>
2021-05-14 18:27:18 +0800

    dota item and hero api

--- src/public/dota.ts
+++ src/public/dota.ts
@@ -1,5 +1,6 @@
 import { Context } from "koa";
 import { leagues, news, newsDetail, topNews } from "../admin/dota";
+import { db } from "../mongo";
 import { getDataResult, getErrorResult } from "../utils";
 
 export function getNews(ctx: Context) {
@@ -18,7 +19,50 @@ export function getNewsDetail(ctx: Context) {
     ctx.body = detail ? getDataResult(detail) : getErrorResult('detail not exist');
 }
 
-
 export function getLeagues(ctx: Context) {
     ctx.body = getDataResult(leagues);
+}
+
+export async function getItems(ctx: Context) {
+    const itemsDb = db.collection('mongoDotaItem');
+    const result = await itemsDb.find(null, {
+        projection: {
+            "type": 1,
+            "name": 1,
+            "img": 1,
+            "cost": 1,
+            "cname": 1
+        }
+    }).toArray();
+    ctx.body = getDataResult(result);
+}
+
+export async function getItemDetail(ctx: Context) {
+    if (!ctx.params.id) ctx.throw(400, 'id required');
+    const itemsDb = db.collection('mongoDotaItem');
+    const result = await itemsDb.findOne({ _id: ctx.params.id }, {
+        projection: {
+            "_id": 0,
+            "type": 0,
+            "name": 0,
+            "img": 0,
+            "cost": 0,
+            "cname": 0,
+            "_class": 0
+        }
+    });
+    ctx.body = getDataResult(result);
+}
+
+export async function getHeros(ctx: Context) {
+    const itemsDb = db.collection('mongoDota2Hero');
+    const result = await itemsDb.find().toArray();
+    ctx.body = getDataResult(result);
+}
+
+export async function getHeroDetail(ctx: Context) {
+    if (!ctx.params.id) ctx.throw(400, 'id required');
+    const itemsDb = db.collection('mongoHeroDetail');
+    const result = await itemsDb.findOne({ _id: ctx.params.id });
+    ctx.body = getDataResult(result);
 }
--- src/router.ts
+++ src/router.ts
@@ -1,36 +1,42 @@
 import * as Router from '@koa/router';
 import * as jwt from 'koa-jwt';
 
-import { getConfig } from './public/common';
-import { deleteConfig, setConfig } from './admin/common';
-import { login } from './auth';
-import { clearNews, putLeagues, setNews, setNewsDetail, setTopNews } from './admin/dota';
-import { getLeagues, getNews, getNewsDetail } from './public/dota';
+import * as common from './public/common';
+import * as admin_common from './admin/common';
+import * as auth from './auth';
+import * as admin_dota from './admin/dota';
+import * as dota from './public/dota';
 
 export default function getRouter(): Router {
     const router = new Router({ prefix: '/node' });
 
-    router.get('/config', getConfig);
+    router.get('/config', common.getConfig);
 
-    router.get('/dota/news', getNews);
-    router.get('/dota/news/:id', getNewsDetail);
+    router.get('/dota/news', dota.getNews);
+    router.get('/dota/news/:id', dota.getNewsDetail);
 
-    router.get('/dota/leagues', getLeagues);
+    router.get('/dota/leagues', dota.getLeagues);
 
-    router.get('/login', login);
+    router.get('/dota/items', dota.getItems);
+    router.get('/dota/items/:id', dota.getItemDetail);
+
+    router.get('/dota/heros', dota.getHeros);
+    router.get('/dota/heros/:id', dota.getHeroDetail);
+
+    router.get('/login', auth.login);
 
     // Middleware below this line is only reached if JWT token is valid
     router.use(jwt({ secret: process.env.JWT_SECRET }));
 
-    router.put('/admin/config', setConfig);
-    router.delete('/admin/config', deleteConfig);
+    router.put('/admin/config', admin_common.setConfig);
+    router.delete('/admin/config', admin_common.deleteConfig);
 
-    router.put('/admin/dota/news', setNews);
-    router.put('/admin/dota/news/:id', setNewsDetail);
-    router.delete('/admin/dota/news', clearNews);
-    router.put('/admin/dota/top', setTopNews);
+    router.put('/admin/dota/news', admin_dota.setNews);
+    router.put('/admin/dota/news/:id', admin_dota.setNewsDetail);
+    router.delete('/admin/dota/news', admin_dota.clearNews);
+    router.put('/admin/dota/top', admin_dota.setTopNews);
 
-    router.put('/admin/dota/leagues', putLeagues);
+    router.put('/admin/dota/leagues', admin_dota.putLeagues);
 
     return router;
 }
